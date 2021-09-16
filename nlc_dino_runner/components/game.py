c3401578aba14f178a7e0bc76faa2ebe346fd2c8
JPDArequipaP <juanpa.arequipa@gmail.com>
2021-09-16 19:50:59 -0400

    Clase 6 - Implemetacion de PowerUp, PowerUpManager y Shield

--- nlc_dino_runner/components/game.py
+++ nlc_dino_runner/components/game.py
@@ -1,10 +1,10 @@
 import pygame
 
+from nlc_dino_runner.components.powerups.power_up_manager import PowerUpManager
 from nlc_dino_runner.utils import text_utils
 from nlc_dino_runner.components.obstacles.obstaclesManager import ObstaclesManager
 from nlc_dino_runner.utils.constants import TITLE, ICON, SCREEN_WIDTH, SCREEN_HEIGHT, BG, FPS
 from nlc_dino_runner.components.dinosaur import Dinosaur
-from nlc_dino_runner.utils.constants import RUNNING
 
 
 class Game:
@@ -20,12 +20,14 @@ class Game:
         self.y_pos_bg = 380
         self.player = Dinosaur()
         self.obstacle_manager = ObstaclesManager()
+        self.power_up_manager = PowerUpManager()
         self.points = 0
         self.running = True
         self.death_count = 0
 
     def run(self):
         self.obstacle_manager.reset_obstacles()
+        self.power_up_manager.reset_power_ups(self.points)
         self.points = 0
         self.playing = True
         #GAME LOOP: event, update, draw
@@ -33,7 +35,6 @@ class Game:
             self.event()
             self.update()
             self.draw()
-        #pygame.quit()
 
     def event(self):
         for event in pygame.event.get():
@@ -44,6 +45,7 @@ class Game:
         user_input = pygame.key.get_pressed()
         self.player.update(user_input)
         self.obstacle_manager.update(self)
+        self.power_up_manager.update(self.points, self.game_speed, self.player)
 
     def draw(self):
         self.clock.tick(FPS)
@@ -52,6 +54,7 @@ class Game:
         self.draw_background()
         self.player.draw(self.screen)
         self.obstacle_manager.draw(self.screen)
+        self.power_up_manager.draw(self.screen)
 
         pygame.display.update()
         pygame.display.flip()
@@ -62,6 +65,8 @@ class Game:
             self.game_speed += 1
         score_element, score_element_rect = text_utils.get_score_element(self.points)
         self.screen.blit(score_element, score_element_rect)
+
+    #def check_invincibility(self):
 
     def draw_background(self):
         image_width = BG.get_width()
